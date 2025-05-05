a199c974573c5d107af3b9691ab8326291a7cef3
Ekant Verma <ekantvermasolanki@gmail.com>
2025-05-05 11:08:49 +0530

    changes done

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,42 +1,66 @@
-import React from 'react'
-import classes from "./ContactUs.module.css"
-import Button from "../common/Button/Button"
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+import classes from './ContactUs.module.css';
+import Button from '../common/Button/Button';
 
 const ContactUs = () => {
-  return (
-    <>
-      <div className={classes.background}>
-        {/* <div className={classes.registerForm}> */}
-        <div className={classes.card}>
-          <div className={classes.card2}>
-              <h1 className={classes.heading}>Contact Us</h1>
-              <div className={classes.userData}>
-                <div className={classes.input_container}>
-                  <label className={classes.input_label}>
-                    Name<span className={classes.mandatory}>*</span>
-                  </label>
-                  <input className={classes.input}/>
-                </div>
-                <div className={classes.input_container}>
-                  <label className={classes.input_label}>
-                    Email<span className={classes.mandatory}>*</span>
-                  </label>
-                  <input className={classes.input}/>
-                </div>
-                <div className={classes.input_container}>
-                  <label className={classes.input_label}>
-                    Message<span className={classes.mandatory}>*</span>
-                  </label>
-                  <textarea className={classes.message}>Enter text Here</textarea>
-                </div>
-              </div>
-              <div className={classes.button}><Button label="Send"/></div>
-            </div>
-        </div>
-        {/* </div> */}
-      </div>
-    </>
-  )
-}
+  const [state, handleSubmit] = useForm("xkgnovre"); 
+  if (state.succeeded) {
+    return <p className={classes.successMessage}>Thanks for reaching out!</p>;
+  }
 
-export default ContactUs
+  return (
+    <div className={classes.background}>
+      <div className={classes.card}>
+        <div className={classes.card2}>
+          <h1 className={classes.heading}>Contact Us</h1>
+          <form onSubmit={handleSubmit} className={classes.userData}>
+            <div className={classes.input_container}>
+              <label className={classes.input_label}>
+                Name<span className={classes.mandatory}>*</span>
+              </label>
+              <input 
+                id="name" 
+                type="text" 
+                name="name"
+                className={classes.input} 
+                required 
+              />
+            </div>
+            <div className={classes.input_container}>
+              <label className={classes.input_label}>
+                Email<span className={classes.mandatory}>*</span>
+              </label>
+              <input 
+                id="email" 
+                type="email" 
+                name="email" // Check that 'email' matches the expected field name
+                className={classes.input} 
+                required 
+              />
+              <ValidationError prefix="Email" field="email" errors={state.errors} />
+            </div>
+            <div className={classes.input_container}>
+              <label className={classes.input_label}>
+                Message<span className={classes.mandatory}>*</span>
+              </label>
+              <textarea 
+                id="message" 
+                name="message" // Check that 'message' matches the expected field name
+                className={classes.message} 
+                placeholder="Enter text here" 
+                required 
+              />
+              <ValidationError prefix="Message" field="message" errors={state.errors} />
+            </div>
+            <div className={classes.button}>
+              <Button label="Send" disabled={state.submitting} />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
